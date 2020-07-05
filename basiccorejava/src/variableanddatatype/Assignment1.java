a3604f716fd92b79424dd741f8f6dcb45d8a2e38
anandpashiney <anandpbond@gmail.com>
2020-07-04 21:02:24 -0700

    committing 5 files updating 3 files , 1 delete and 1 add

--- basiccorejava/src/variableanddatatype/Assignment1.java
+++ basiccorejava/src/variableanddatatype/Assignment1.java
@@ -4,16 +4,16 @@ public class Assignment1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int a=10;
-int b=20;
-int temp=0;
-temp=a;
-a=b;
-b=temp;
+		int a = 10;
+		int b = 20;
+		int temp = 0;
+		temp = a;
+		a = b;
+		b = temp;
 
-
-System.out.println(b);
-System.out.println(a);
+		System.out.println();
+		System.out.println(b);
+		System.out.println(a);
 	}
 
 }
